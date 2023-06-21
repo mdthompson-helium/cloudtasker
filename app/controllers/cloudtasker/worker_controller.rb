11c3bd22cd4a029444ade24cca00dc8cca44a1b3
Matt Thompson <matt@heliumdev.com>
2023-06-21 15:54:39 -0700

    don't raise verify_authenticity_token if not defined

--- app/controllers/cloudtasker/worker_controller.rb
+++ app/controllers/cloudtasker/worker_controller.rb
@@ -4,7 +4,7 @@ module Cloudtasker
   # Handle execution of workers
   class WorkerController < ActionController::Base
     # No need for CSRF verification on API endpoints
-    skip_before_action :verify_authenticity_token
+    skip_before_action :verify_authenticity_token, raise: false
 
     # Authenticate all requests.
     before_action :authenticate!
